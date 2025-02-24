cd4312fa3f58ff76a256b602f64fa64d0fbae44c
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-02-24 09:27:21 +0100

    Add swagger and scalar docs to the backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+
+  const config = new DocumentBuilder().setTitle('API').setDescription('API description').setVersion('1.0').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.use(
+    '/scalar',
+    apiReference({
+      spec: {
+        content: documentFactory,
+      },
+    })
+  );
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
